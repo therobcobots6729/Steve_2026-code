c936563a64d2358f0d4a1279b7233e19d92816c5
ATurner8133 <156738528+ATurner8133@users.noreply.github.com>
2026-02-07 11:14:30 -0500

    Update limelight.java

--- Steve2026/src/main/java/frc/robot/subsystems/limelight.java
+++ Steve2026/src/main/java/frc/robot/subsystems/limelight.java
@@ -135,6 +135,7 @@ public class Limelight extends SubsystemBase {
     SmartDashboard.putNumber("ty1", ty.getDouble(0.0));
     SmartDashboard.putNumber("tid1", tid1.getDouble(0.0));
     SmartDashboard.putNumber("target Shooter velocity", speed()*60);
+    SmartDashboard.putNumber("distance", distance());
 
     // This method will be called once per scheduler run
   }
