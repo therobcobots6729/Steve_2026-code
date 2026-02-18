c45bbf78c83e62a4b00c7ab06023f6c2ccdd2ff5
ATurner8133 <156738528+ATurner8133@users.noreply.github.com>
2026-02-17 19:25:11 -0500

    Update Turret.java

--- Steve2026/src/main/java/frc/robot/subsystems/Turret.java
+++ Steve2026/src/main/java/frc/robot/subsystems/Turret.java
@@ -55,14 +55,14 @@ public class Turret extends SubsystemBase {
     double commandedError = error;
 
     // limit logic with forced wrap
-    if (turretAngle >= 90 && commandedError > 0) {
+    /*if (turretAngle >= 90 && commandedError > 0) {
         // blocked going positive → go full turn negative
         commandedError = error - 340;
     }
     else if (turretAngle <= -90 && commandedError < 0) {
         // blocked going negative → go full turn positive
         commandedError = error + 340;
-    }
+    }*/
 
     double output = controller.calculate(0-commandedError);
     output = MathUtil.clamp(output, -1.0, 1.0);
