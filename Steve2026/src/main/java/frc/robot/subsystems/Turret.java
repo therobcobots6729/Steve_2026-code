0d906e526f457d3a1de8bcffc6d5df5aae4908bd
ATurner8133 <156738528+ATurner8133@users.noreply.github.com>
2026-02-18 20:28:38 -0500

    Update Turret.java

--- Steve2026/src/main/java/frc/robot/subsystems/Turret.java
+++ Steve2026/src/main/java/frc/robot/subsystems/Turret.java
@@ -39,7 +39,7 @@ public class Turret extends SubsystemBase {
     controller.setTolerance(1.0);
     encoder = turnMotor.getEncoder();
     absAngle = new Encoder(0,1,false,Encoder.EncodingType.k2X);
-    absAngle.setDistancePerPulse(.01578);
+    absAngle.setDistancePerPulse(.01578125);
       
   }
    public double getAngle(){
