c87dbd376baae7d38f9eb9810ee0b7f38f937c89
ATurner8133 <156738528+ATurner8133@users.noreply.github.com>
2026-03-05 21:13:21 -0500

    Update LimelightHelpers.java

--- Steve2026/src/main/java/frc/robot/subsystems/LimelightHelpers.java
+++ Steve2026/src/main/java/frc/robot/subsystems/LimelightHelpers.java
@@ -1,6 +1,6 @@
 //LimelightHelpers v1.14 (REQUIRES LLOS 2026.0 OR LATER)
 
-package frc.robot;
+package frc.robot.subsystems;
 
 import edu.wpi.first.networktables.DoubleArrayEntry;
 import edu.wpi.first.networktables.NetworkTable;
